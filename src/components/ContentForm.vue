5d6de2c4af6aa82f1be9b507974d2a6e56847b59
DoktorShift <106493492+DoktorShift@users.noreply.github.com>
2025-08-03 21:17:36 +0200

    Updated ContentForm.vue

--- src/components/ContentForm.vue
+++ src/components/ContentForm.vue
@@ -851,7 +851,6 @@ Write naturally and let your thoughts flow. Your content will be published as a 
     </div>
   </div>
 </template>
-</template>
 
 <style scoped>
 /* Custom scrollbar for preview panel */
