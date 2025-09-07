61ba53008c56694536c00ea3852a5d3ac1b2e52f
DoktorShift <106493492+DoktorShift@users.noreply.github.com>
2025-09-07 15:37:24 +0200

    Fix missing watch import in ErrorBoundary

--- src/components/ErrorBoundary.vue
+++ src/components/ErrorBoundary.vue
@@ -28,7 +28,7 @@
 </template>
 
 <script setup>
-import { ref, onErrorCaptured, inject } from 'vue'
+import { ref, onErrorCaptured, inject, watch } from 'vue'
 import { IconAlertTriangle } from '@iconify-prerendered/vue-tabler'
 
 const error = ref(null)
