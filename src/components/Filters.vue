6a3250e489a423232a788f28dae7235d8d1ab717
DoktorShift <106493492+DoktorShift@users.noreply.github.com>
2025-09-14 21:37:38 +0200

    Fix Filters Component Error

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -59,7 +59,7 @@ const clearAllFilters = () => {
           <IconSearch class="absolute left-3 top-3.5 w-4 h-4 text-gray-400" />
           <!-- Clear search button -->
           <button
-            v-if="searchQuery.value"
+            v-if="searchQuery"
             @click="searchQuery = ''"
             class="absolute right-3 top-3.5 text-gray-400 hover:text-gray-600 transition-colors"
           >
